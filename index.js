523943612817c8bd8536f262f8a4343df5199c9f
Hem Kishore Pradhan <hemkishoresoa@gmail.com>
2024-01-30 06:00:56 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import path, { resolve } from "path";
+import bodyParser from "body-parser";
+const app = express();
+const port = 3000;
+const users = [];
+app.use(express.static(path.join(path.resolve(), "public")));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.render("index.ejs", { name: "hemkishore" });
+});
+app.post("/submit", (req, res) => {
+  users.push(req.body);
+  res.redirect("/");
+});
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+app.listen(port, () => {
+  console.log("Server is running...");
+});
